0ec36c1bc200a6adc30836d6fbe6157d128d5332
azeem-sketch <azeemsaleem859@gmail.com>
2026-02-07 00:04:47 +0500

    Fix Vercel startup script

--- start.bat
+++ start.bat
@@ -6,7 +6,7 @@ taskkill /F /IM python.exe /T 2>nul
 echo Starting Evolution of Todo...
 
 start cmd /k "echo Starting Backend... && cd evolution-of-todo\backend && venv\Scripts\python run_server.py"
-start cmd /k "echo Starting Frontend... && cd evolution-of-todo\frontend && npm run dev -- -H 127.0.0.1"
+start cmd /k "echo Starting Frontend... && cd evolution-of-todo\frontend-v2 && npm run dev"
 
 echo Both servers are starting in separate windows.
 echo Frontend: http://127.0.0.1:3000
